d60e265ed3678eefac0ffc503e5c771887b23ae0
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2015-10-21 18:11:08 -0700

    Initial OpenTK window

--- SimpleLightingSample/SimpleLights.cs
+++ SimpleLightingSample/SimpleLights.cs
@@ -2,13 +2,54 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using OpenTK;
+using OpenTK.Input;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
 
 namespace SimpleLightingSample
 {
     class SimpleLights
     {
+        public static SimpleLights Instance = null;
+
         static void Main(string[] args)
         {
+            Instance = new SimpleLights();
+            Instance.Run();
+        }
+
+        public GameWindow Window = null;
+
+        public bool InputEnabled = false;
+
+        public void Run()
+        {
+            Console.WriteLine("Load window...");
+            Window = new GameWindow(800, 600, GraphicsMode.Default, "Simple Lighting Sample", GameWindowFlags.Default, DisplayDevice.Default, 4, 3, GraphicsContextFlags.ForwardCompatible);
+            Window.Load += Window_Load;
+            Window.UpdateFrame += Window_UpdateFrame;
+            Window.RenderFrame += Window_RenderFrame;
+            Window.Run();
+            Console.WriteLine("End program!");
+        }
+
+        private void Window_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("Load OpenGL...");
+            GL.ClearColor(Color4.White);
+            GL.Disable(EnableCap.DepthTest);
+        }
+
+        private void Window_UpdateFrame(object sender, FrameEventArgs e)
+        {
+            InputEnabled = Window.Focused;
+        }
+
+        private void Window_RenderFrame(object sender, FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            Window.SwapBuffers();
         }
     }
 }
