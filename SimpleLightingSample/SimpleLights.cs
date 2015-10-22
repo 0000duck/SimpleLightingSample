d1a3065e6c4f7537211f864d73a2ede802091233
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2015-10-21 17:59:57 -0700

    Empty C# app

---
+++ SimpleLightingSample/SimpleLights.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleLightingSample
+{
+    class SimpleLights
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
